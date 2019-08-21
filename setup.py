4919e207327a49f52c90626bc6596e8bf9febb0f
Gabriel Laden <gabrielnexus6p@gmail.com>
2019-08-21 00:30:32 -0700

    update package

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     platforms='any',
     install_requires=[
         'Flask-AppBuilder>=1.5.0',
-        'Flask-OIDC@https://github.com/puiterwijk/flask-oidc.git#egg=Flask-OIDC',
+        'Flask-OIDC>=1.4.0',
         'Flask-Admin>=1.4.1'
     ],
     tests_require=[
